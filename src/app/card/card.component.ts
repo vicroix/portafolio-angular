fe398d3c9bb61892c6312a22ab69246b3d314b6b
VictorMedac <vbj0003@alu.medac.es>
2025-03-30 01:28:16 +0100

    portafolio 1.0v

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input} from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-card',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
@@ -11,4 +12,5 @@ export class CardComponent {
   @Input() imageSrc: string = '';
   @Input() getTitle: string = 'Título por defecto';
   @Input() id: string = '';
+  botones = 'py-2 px-4 rounded-xl p-2 shadow-sm shadow-gray-500 m-4 w-45 bg-[rgb(176,62,255)] hover:bg-[rgb(189,95,255)] active:bg-[rgb(166,91,219)]  text-white font-medium';
 }
--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -13,6 +13,8 @@ import { RouterModule } from '@angular/router';
 })
 export class DetailsComponent implements OnInit {
   @Input() fontH2 = '';
+  botonGit1 = 'flex justify-center bg-[rgb(176,62,255)] hover:bg-[rgb(189,95,255)] active:bg-[rgb(166,91,219)] w-35 shadow-sm shadow-gray-500 rounded-xl mt-20';
+  botonGit2 = 'flex justify-center bg-[rgb(62,255,175)] hover:bg-[rgb(104,255,193)] active:bg-[rgb(86,226,169)] w-35 shadow-sm shadow-gray-500 rounded-xl mt-5';
   //Cargar objeto de arrays de "public/proyectos.json"
   proyecto: any;
 
