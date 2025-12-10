ddba06e5f2b100cb4de387953859ce54c40fd0db
vicroix <victor.batista.jaldon@gmail.com>
2025-12-10 20:09:39 +0100

    cambio diseño y responsive completado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { MidescripcionComponent } from './midescripcion/midescripcion.component'
 import { FooterComponent } from './footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { AppearOnScrollDirective } from './directives/appear-on-scroll.directive';
-import { BackgroundAnimationService } from './background-animation.service';
 
 @Component({
   selector: 'app-root',
@@ -17,12 +16,5 @@ export class AppComponent {
   fontH2 = 'text-lg text-[20px] cambio-color-volver';
   title = 'Pagina_angular'
 
-  constructor(private bgAnimation: BackgroundAnimationService){}
-
-  ngOnInit(): void{
-    setInterval(()=>{
-      this.bgAnimation.pixel();
-    }, 150)
-  }
 }
 
